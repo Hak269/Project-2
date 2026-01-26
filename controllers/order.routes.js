ec50b8c145b6139f0222ecd93c9ed22e3d250f40
Hak269 <h2000.alawi@gmail.com>
2026-01-26 19:59:00 +0300

    finished stage one of create new order

--- controllers/order.routes.js
+++ controllers/order.routes.js
@@ -3,16 +3,19 @@ const Order = require("../models/Order")
 
 router.get('/',(req,res)=>{
     const allOrders = Order.find()
-    res.render('orders.ejs', allOrders)
+    console.log(allOrders)
+    res.render('orders.ejs', {allOrders: allOrders})
 })
 
-router.post('/new',(req,res)=>{
-    const addNew = Order.create(req.body)
-    res.redirect("Orders/")
+router.post('/create', async (req,res)=>{
+    console.log(req.body)
+    const addNew = await Order.create(req.body)
+    console.log(addNew)
+    res.redirect("/")
 })
 
 router.get('/create',(req,res)=>{
-    res.render('orders.ejs')
+    res.render('newOrder.ejs')
 })
 
 
