1b102534ddaa116d5587c44da945d43caedfbe4b
Hak269 <h2000.alawi@gmail.com>
2026-01-28 20:45:58 +0300

    finished edit and update for recipes

--- controllers/recipes.routes.js
+++ controllers/recipes.routes.js
@@ -3,7 +3,8 @@ const Recipe = require("../models/Recipe")
 const Inventory = require("../models/Inventory")
 
 router.get('/', async(req,res)=>{
-    const recipes = await Recipe.find()
+    const recipes = await Recipe.find().populate("ingredients")
+    console.log(recipes)
     res.render('recipes.ejs', {recipes: recipes})
 })
 
@@ -18,13 +19,43 @@ router.post('/create', async (req,res)=>{
         .filter(step => step.length > 0)
     };
     const addNew = await Recipe.create(recipe)
-    res.redirect("/")
+    res.redirect("/recipe")
 })
 
-router.get('/create',(req,res)=>{
-    const inventory = Inventory.find()
+router.get('/create', async (req,res)=>{
+    const inventory = await Inventory.find()
     res.render('newRecipe.ejs', {inventory: inventory})
 })
 
+
+router.get('/:id/edit', async(req,res)=>{
+    const inventory = await Inventory.find()
+    const recipe = await Recipe.findById(req.params.id).populate('ingredients')
+    res.render('./EditRecipe.ejs', {recipe: recipe, inventory: inventory})
+})
+
+router.post('/:id/edit', async(req,res)=>{
+    const recipe = {
+    recipeName: req.body.recipeName,
+    ingredients: req.body.ingredients,
+    price: req.body.price,
+    instructions: req.body.instructions
+        .split('\n')
+        .map(step => step.trim())
+        .filter(step => step.length > 0)
+    };
+    const editted = await Recipe.findByIdAndUpdate(req.params.id, recipe)
+    res.redirect('/recipe')
+})
+
+router.get('/:id', async(req,res)=>{
+    const recipe = await Recipe.findById(req.params.id).populate('ingredients')
+    res.render('./viewRecipe.ejs', {recipe: recipe})
+})
+
+router.post('/:id/delete', async(req,res)=>{
+    const recipe = await Recipe.findByIdAndDelete(req.params.id)
+    res.redirect('/recipe')
+})
 
 module.exports = router;
