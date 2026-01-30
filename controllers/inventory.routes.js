59f5c4a6688bacfb62d4e044f999341c0f24ca97
Hak269 <h2000.alawi@gmail.com>
2026-01-30 23:53:45 +0300

    Finished update and delete in inventory

--- controllers/inventory.routes.js
+++ controllers/inventory.routes.js
@@ -24,5 +24,31 @@ router.get('/create',(req,res)=>{
     res.render('newItem.ejs')
 })
 
+router.get('/:id/edit', async(req,res)=>{
+    const item = await Inventory.findById(req.params.id)
+    res.render('./editItem.ejs', {item: item})
+})
+
+router.post('/:id/edit', async(req,res)=>{
+    const inventoryItem = {
+        item: req.body.item,
+        category: req.body.category,
+        quantity: Number(req.body.quantity),
+        unit: req.body.unit,
+        menuYN: req.body.menuYN === "true"
+    };
+    const updatedItem = await Inventory.findByIdAndUpdate(req.params.id, inventoryItem)
+    res.redirect('/inventory')
+})
+
+router.get('/:id', async(req,res)=>{
+    const item = await Inventory.findById(req.params.id)
+    res.render('./viewItem.ejs', {item: item})
+})
+
+router.post('/:id/delete', async(req,res)=>{
+    const item = await Inventory.findByIdAndDelete(req.params.id)
+    res.redirect('/inventory')
+})
 
 module.exports = router;
